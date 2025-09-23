1f608e67cf6a025e07e09326e2d0814f7626282f
jeanette <jeanette>
2025-09-23 01:10:55 -0300

    formulario de reserva

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log('Bienvenido a las nuevas Quintas de Ezeiza');
+(function () {
+  const yearElement = document.getElementById('current-year');
+  if (yearElement) {
+    yearElement.textContent = new Date().getFullYear();
+  }
+
+  console.log('Bienvenido a las nuevas Quintas de Ezeiza');
+})();
